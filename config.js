5a451874e1ed3e7d2e3b3c39d08d2cf644688e3e
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2016-10-29 18:07:04 +0100

    Add config file

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    cookie_encryption_password: ``,
+    twitter: {
+        client_id: ``,
+        client_secret: ``
+    },
+    facebook: {
+        client_id: ``,
+        client_secret: ``
+    }
+}
